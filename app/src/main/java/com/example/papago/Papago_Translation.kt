3fdd6d5b816d768877b52a72d4899a2e2acbc5e8
SANGGONLEE <68982398+GONI95@users.noreply.github.com>
2020-08-31 19:43:40 +0900

    papago3

--- app/src/main/java/com/example/papago/Papago_Translation.kt
+++ app/src/main/java/com/example/papago/Papago_Translation.kt
@@ -33,7 +33,7 @@ class Papago_Translation : AppCompatActivity() {
         val mTextWatcher: TextWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                editTextLiveLength.setText("글자 수: ${s!!.length.toString()}")
+                editTextLiveLength.setText( "글자 수: ${s!!.length.toString()}" )
             }
             override fun afterTextChanged(s: Editable?) { }
         }
